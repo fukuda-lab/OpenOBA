917c54679ef5c792bbdf13ebabecc5ddac0074fa
Mateo <maormeno@uc.cl>
2024-01-25 15:31:09 +0900

    Updated environment.yaml to work in MacOS

--- demo.py
+++ demo.py
@@ -7,7 +7,16 @@ import subprocess
 from pathlib import Path
 from bannerclick.config import *
 
-from CMPB_commands import init, CMPBCommand, InitCommand, SubGetCommand, BannerDetectionCommand, CMPDetectionCommand, SetEntryCommand, SaveDatabaseCommand
+from CMPB_commands import (
+    init,
+    CMPBCommand,
+    InitCommand,
+    SubGetCommand,
+    BannerDetectionCommand,
+    CMPDetectionCommand,
+    SetEntryCommand,
+    SaveDatabaseCommand,
+)
 from openwpm.command_sequence import CommandSequence
 from openwpm.commands.browser_commands import GetCommand
 from openwpm.config import BrowserParams, ManagerParams
@@ -17,11 +26,19 @@ from openwpm.task_manager import TaskManager
 # The list of sites that we wish to crawl
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--headless', action='store_true', help="start on headless mode")
-parser.add_argument("--num-browsers", type=int, default=10, help="Number of browser instances")
-parser.add_argument("--num-repetitions", type=int, default=5, help="Number of repetitions per website")
-parser.add_argument("--bannerclick", action="store_true", help="Run banner click custom command")
-parser.add_argument("target_file", type=argparse.FileType("r"), help="File with target websites")
+parser.add_argument("--headless", action="store_true", help="start on headless mode")
+parser.add_argument(
+    "--num-browsers", type=int, default=10, help="Number of browser instances"
+)
+parser.add_argument(
+    "--num-repetitions", type=int, default=5, help="Number of repetitions per website"
+)
+parser.add_argument(
+    "--bannerclick", action="store_true", help="Run banner click custom command"
+)
+parser.add_argument(
+    "target_file", type=argparse.FileType("r"), help="File with target websites"
+)
 
 
 args = parser.parse_args()
@@ -32,7 +49,7 @@ def file_to_list(path):
     file = open(path, "r")
     domains = []
     while True:
-        domain = file.readline().strip('\n')
+        domain = file.readline().strip("\n")
         if not domain:
             break
         if domain == "#":
@@ -44,7 +61,7 @@ def file_to_list(path):
 
 
 def make_url(domain: str, mode=URL_MODE):
-    domain = domain.strip('\n')
+    domain = domain.strip("\n")
     if "https://" in domain or "http://" in domain:
         url = domain
     else:
@@ -63,23 +80,41 @@ if ".csv" in input_file.name:
     if STEP_SIZE == -1:
         STEP_SIZE = sites_csv.shape[0]
     if TIERED300:
-        sites = [make_url(sites_csv.iloc[row].domain) for row in range(0, 0+STEP_SIZE)]
-        sites.extend([make_url(sites_csv.iloc[row].domain) for row in range(1000, 1000 + STEP_SIZE)])
-        sites.extend([make_url(sites_csv.iloc[row].domain) for row in range(9900, 9900 + STEP_SIZE)])
-    else:     # read from .csv file. from START_POINT to START_POINT + STEP_SIZE
-        sites = [make_url(sites_csv.iloc[row].domain) for row in range(START_POINT, START_POINT + STEP_SIZE)]
-else:    # read from .txt file
+        sites = [
+            make_url(sites_csv.iloc[row].domain) for row in range(0, 0 + STEP_SIZE)
+        ]
+        sites.extend(
+            [
+                make_url(sites_csv.iloc[row].domain)
+                for row in range(1000, 1000 + STEP_SIZE)
+            ]
+        )
+        sites.extend(
+            [
+                make_url(sites_csv.iloc[row].domain)
+                for row in range(9900, 9900 + STEP_SIZE)
+            ]
+        )
+    else:  # read from .csv file. from START_POINT to START_POINT + STEP_SIZE
+        sites = [
+            make_url(sites_csv.iloc[row].domain)
+            for row in range(START_POINT, START_POINT + STEP_SIZE)
+        ]
+else:  # read from .txt file
     all_sites = [make_url(domain) for domain in args.target_file.readlines()]
-    sites = all_sites[START_POINT:START_POINT+STEP_SIZE]
-
+    sites = all_sites[START_POINT : START_POINT + STEP_SIZE]
 
 
 print("browsers ", args.num_browsers)
 manager_params = ManagerParams(num_browsers=args.num_browsers)
 if HEADLESS:
-    browser_params = [BrowserParams(display_mode="headless") for _ in range(args.num_browsers)]
+    browser_params = [
+        BrowserParams(display_mode="headless") for _ in range(args.num_browsers)
+    ]
 else:
-    browser_params = [BrowserParams(display_mode="native") for _ in range(args.num_browsers)]
+    browser_params = [
+        BrowserParams(display_mode="native") for _ in range(args.num_browsers)
+    ]
 
 # Update browser configuration (use this for per-browser settings)
 for browser_param in browser_params:
@@ -96,7 +131,9 @@ for browser_param in browser_params:
     # Record DNS resolution
     browser_param.dns_instrument = False
     if MOBILE_AGENT:
-        browser_param.prefs["general.useragent.override"] = "Mozilla/5.0 (Android 12; Mobile; rv:68.0) Gecko/68.0 Firefox/93.0"
+        browser_param.prefs["general.useragent.override"] = (
+            "Mozilla/5.0 (Android 12; Mobile; rv:68.0) Gecko/68.0 Firefox/93.0"
+        )
     browser_param.extension_enabled = XPI
 
 
@@ -119,14 +156,24 @@ try:
     original_umask = os.umask(0)
     if not os.path.exists(data_dir):
         os.makedirs(data_dir, 0o0777)
-    if not os.path.exists(data_dir+"/csv"):
-        os.makedirs(data_dir+"/csv", 0o0777)
+    if not os.path.exists(data_dir + "/csv"):
+        os.makedirs(data_dir + "/csv", 0o0777)
 finally:
     os.umask(original_umask)
 
 init(HEADLESS, input_file.name, args.num_browsers, args.num_repetitions)
+
+sites = [
+    "http://vogue.com",
+    "http://elle.fr",
+    "http://marieclaire.com",
+    # "http://cosmopolitan.com",
+    # "http://elle.com",
+    # "http://zara.com",
+    # "http://wwd.com",
+]
 start_time = datetime.now()
-with open(log_file, 'a+') as f:
+with open(log_file, "a+") as f:
     init_str = "started at: " + start_time.strftime("%H-%M-%S").__str__()
     print(init_str, file=f)
 # Commands time out by default after 60 seconds
@@ -136,7 +183,6 @@ with TaskManager(
     SQLiteStorageProvider(Path(data_dir + "/crawl-data.sqlite")),
     None,
 ) as manager:
-
     index = 0
 
     for site in sites:
@@ -145,7 +191,8 @@ with TaskManager(
 
         def callback(success: bool, val: str = site) -> None:
             print(
-                f"CommandSequence for {val} ran {'successfully' if success else 'unsuccessfully'}"
+                f"CommandSequence for {val} ran"
+                f" {'successfully' if success else 'unsuccessfully'}"
             )
 
         for _ in range(args.num_repetitions):
@@ -161,42 +208,83 @@ with TaskManager(
                 # manager.execute_command_sequence(command_sequence)
 
                 # 1. accept
-                command_sequence = CommandSequence(site, site_rank=index , callback=callback, reset=True)
+                command_sequence = CommandSequence(
+                    site, site_rank=index, callback=callback, reset=True
+                )
                 command_sequence.append_command(
-                    CMPBCommand(url=site, sleep=SLEEP_TIME, index=index, timeout=TIME_OUT, choice=1),
-                    timeout=TIME_OUT * 11)
+                    CMPBCommand(
+                        url=site,
+                        sleep=SLEEP_TIME,
+                        index=index,
+                        timeout=TIME_OUT,
+                        choice=2,
+                    ),
+                    timeout=TIME_OUT * 11,
+                )
                 manager.execute_command_sequence(command_sequence)
 
                 if COMPLETE_RUN:
                     # 2. accept the banner
-                    command_sequence = CommandSequence(site, site_rank=index + OFFSET_ACCEPT, callback=callback, reset=True)
-                    command_sequence.append_command(CMPBCommand(url=site, sleep=SLEEP_TIME, index=index + OFFSET_ACCEPT, timeout=TIME_OUT, choice=1), timeout=TIME_OUT * 11)
+                    command_sequence = CommandSequence(
+                        site,
+                        site_rank=index + OFFSET_ACCEPT,
+                        callback=callback,
+                        reset=True,
+                    )
+                    command_sequence.append_command(
+                        CMPBCommand(
+                            url=site,
+                            sleep=SLEEP_TIME,
+                            index=index + OFFSET_ACCEPT,
+                            timeout=TIME_OUT,
+                            choice=2,
+                        ),
+                        timeout=TIME_OUT * 11,
+                    )
                     manager.execute_command_sequence(command_sequence)
 
                     # 3. reject the banner
-                    command_sequence = CommandSequence(site, site_rank=index + OFFSET_REJECT, callback=callback, reset=True)
-                    command_sequence.append_command(CMPBCommand(url=site, sleep=SLEEP_TIME, index=index + OFFSET_REJECT, timeout=TIME_OUT, choice=2), timeout=TIME_OUT * 11)
+                    command_sequence = CommandSequence(
+                        site,
+                        site_rank=index + OFFSET_REJECT,
+                        callback=callback,
+                        reset=True,
+                    )
+                    command_sequence.append_command(
+                        CMPBCommand(
+                            url=site,
+                            sleep=SLEEP_TIME,
+                            index=index + OFFSET_REJECT,
+                            timeout=TIME_OUT,
+                            choice=2,
+                        ),
+                        timeout=TIME_OUT * 11,
+                    )
                     manager.execute_command_sequence(command_sequence)
-
 
             # Run the code without bannerclick, e.g. for consistency measurements
             # "Shivani's algorithm"
             else:
 
                 # Parallelize sites over all number of browsers set above.
-                command_sequence = CommandSequence(site, site_rank=index, callback=callback, reset=True)
-                command_sequence.append_command(GetCommand(url=site, sleep=SLEEP_TIME), timeout=TIME_OUT)
+                command_sequence = CommandSequence(
+                    site, site_rank=index, callback=callback, reset=True
+                )
+                command_sequence.append_command(
+                    GetCommand(url=site, sleep=SLEEP_TIME), timeout=TIME_OUT
+                )
                 manager.execute_command_sequence(command_sequence)
-
 
             index += 1
 
     finish_time = datetime.now()
     completion_time = finish_time - start_time
-    with open(log_file, 'a+') as f:
-        init_str = "finished at: " + finish_time.strftime("%H-%M-%S").__str__() + "\ncompletion time(min): " + str(
-            completion_time.total_seconds() / 60)
+    with open(log_file, "a+") as f:
+        init_str = (
+            "finished at: "
+            + finish_time.strftime("%H-%M-%S").__str__()
+            + "\ncompletion time(min): "
+            + str(completion_time.total_seconds() / 60)
+        )
         print(init_str, file=f)
-    time.sleep(TIME_OUT*2)
-
-
+    time.sleep(TIME_OUT * 2)
