5cfe3e1cc5143100f88a4540946d9f3326dba0d3
Mateo <maormeno@uc.cl>
2024-02-07 19:23:36 +0900

    ready to run 3 choices

--- oba/input_run_files/style_and_fashion_experiment/do_nothing/2_create_experiment.py
+++ oba/input_run_files/style_and_fashion_experiment/do_nothing/2_create_experiment.py
@@ -3,10 +3,29 @@
 from oba_crawler import OBAMeasurementExperiment
 
 
+# fashion_experiment = OBAMeasurementExperiment(
+#     "style_and_fashion_experiment_do_nothing",
+#     True,
+#     do_clean_runs=False,
+#     cookie_banner_action=0,
+# )
+
+# # We get the most popular pages by category that were confidently classified and with cookie banner presence (we can access the ones that have cookie banner presence)
+# fashion_experiment.set_training_pages_by_category(
+#     category="Style & Fashion",
+#     size=50,
+#     confident=1,
+#     cookie_banner_checked=1,
+#     cookie_banner_presence=1,
+# )
+
+# fashion_experiment.start(minutes=5)
+
+
 fashion_experiment = OBAMeasurementExperiment(
     "style_and_fashion_experiment_do_nothing",
     True,
-    do_clean_runs=False,
+    do_clean_runs=True,
     cookie_banner_action=0,
 )
 
@@ -19,4 +38,4 @@ fashion_experiment.set_training_pages_by_category(
     cookie_banner_presence=1,
 )
 
-fashion_experiment.start(minutes=5)
+fashion_experiment.start(8)
--- oba/input_run_files/style_and_fashion_experiment/reject/2_create_experiment.py
+++ oba/input_run_files/style_and_fashion_experiment/reject/2_create_experiment.py
@@ -3,10 +3,28 @@
 from oba_crawler import OBAMeasurementExperiment
 
 
+# fashion_experiment = OBAMeasurementExperiment(
+#     "style_and_fashion_experiment_reject",
+#     True,
+#     do_clean_runs=False,
+#     cookie_banner_action=2,
+# )
+
+# # We get the most popular pages by category that were confidently classified and with cookie banner presence (we can access the ones that have cookie banner presence)
+# fashion_experiment.set_training_pages_by_category(
+#     category="Style & Fashion",
+#     size=50,
+#     confident=1,
+#     cookie_banner_checked=1,
+#     cookie_banner_presence=1,
+# )
+
+# fashion_experiment.start(minutes=5)
+
 fashion_experiment = OBAMeasurementExperiment(
     "style_and_fashion_experiment_reject",
     True,
-    do_clean_runs=False,
+    do_clean_runs=True,
     cookie_banner_action=2,
 )
 
@@ -19,4 +37,4 @@ fashion_experiment.set_training_pages_by_category(
     cookie_banner_presence=1,
 )
 
-fashion_experiment.start(minutes=5)
+fashion_experiment.start(8)
